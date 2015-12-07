84ccc6a1c5ca04f5e31e968862dd8e75d9476ee1
pavankumar patnala <pavankumar patnala>
2015-12-07 23:24:36 +0530

    pavan

---
+++ MYSQL_POST.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import java.net.*;
+import java.applet.*;
+import java.security.*;
+public class MYSQL_POST {
+    public static void main(String r[]){
+    HttpURLConnection conn=null;
+    try{
+    URL url=new URL("http://localhost/datapost.php");
+    String agent="Applet";
+    String query="query=" + r[0];
+    String type="application/x-www-form-urlencoded";
+    conn=(HttpURLConnection)url.openConnection();
+    conn.setDoInput(true);
+    conn.setDoOutput(true);
+    conn.setRequestMethod("POST");
+    conn.setRequestProperty( "User-Agent", agent );
+    conn.setRequestProperty( "Content-Type", type );
+    conn.setRequestProperty( "Content-Length", ""+query.length());
+
+    OutputStream out=conn.getOutputStream();
+    out.write(query.getBytes());
+    BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+    String inputLine;
+    while((inputLine=in.readLine())!=null){
+        System.out.print(inputLine+"\n");
+    };
+    in.close();
+    int rc = conn.getResponseCode();
+    System.out.print("Response Code = "+rc+"\n");
+    String rm=conn.getResponseMessage();
+    System.out.print("Response Message = "+rm+"\n");
+    }catch(Exception e){
+    e.printStackTrace();
+    }finally{
+    conn.disconnect();
+    }
+    }
+
+    }
